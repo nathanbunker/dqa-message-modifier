350ac2c5b952d03d7aa22e10c479d9fd62ce8b7d
Nathan Bunker <Nathan.Bunker@gmail.com>
2017-01-04 17:31:56 +0100

    Updated test cases

--- src/main/java/org/immregistries/dqa/message_modifier/ModifierService.java
+++ src/main/java/org/immregistries/dqa/message_modifier/ModifierService.java
@@ -1,5 +1,7 @@
 package org.immregistries.dqa.message_modifier;
 
+import java.util.concurrent.RunnableScheduledFuture;
+
 public class ModifierService {
   public ModifierService()
   {
@@ -10,4 +12,5 @@ public class ModifierService {
   {
     modifyRequest.setMessageFinal(modifyRequest.getMessageOriginal());
   }
+  
 }
--- src/test/java/org/immregistries/dqa/message_modifier/ModifyRequestTest.java
+++ src/test/java/org/immregistries/dqa/message_modifier/ModifyRequestTest.java
@@ -6,12 +6,41 @@ import org.immregistries.dqa.message_modifier.ModifyRequest;
 import junit.framework.TestCase;
 
 public class ModifyRequestTest extends TestCase {
-  public void initialTests() {
+  public void testInitial() {
+    // We testing basic commands with references
+    {
+      String messageOriginal = "PID|||Q63W1^^^AIRA-TEST^MR||Holmes^Jeramiah^Z^IV^^^L|Monroe^Arden|20160626|M|||155 Lewis Cir^^Cadmus^MI^49221^USA^P||^PRN^PH^^^517^3004208|";
+      String modificationScript = "PID-5=\"\"";
+      String messageFinal = "PID|||Q63W1^^^AIRA-TEST^MR||^Jeramiah^Z^IV^^^L|Monroe^Arden|20160626|M|||155 Lewis Cir^^Cadmus^MI^49221^USA^P||^PRN^PH^^^517^3004208|";
+      runTest(messageOriginal, modificationScript, messageFinal);
+    }
+    {
+      String messageOriginal = "PID|||Q63W1^^^AIRA-TEST^MR||Holmes^Jeramiah^Z^IV^^^L|Monroe^Arden|20160626|M|||155 Lewis Cir^^Cadmus^MI^49221^USA^P||^PRN^PH^^^517^3004208|";
+      String modificationScript = "PID[1]-5=\"\"";
+      String messageFinal = "PID|||Q63W1^^^AIRA-TEST^MR||Holmes^Jeramiah^Z^IV^^^L|Monroe^Arden|20160626|M|||155 Lewis Cir^^Cadmus^MI^49221^USA^P||^PRN^PH^^^517^3004208|";
+      runTest(messageOriginal, modificationScript, messageFinal);
+    }
+    {
+      String messageOriginal = "PID|||Q63W1^^^AIRA-TEST^MR||Holmes^Jeramiah^Z^IV^^^L|Monroe^Arden|20160626|M|||155 Lewis Cir^^Cadmus^MI^49221^USA^P||^PRN^PH^^^517^3004208|";
+      String modificationScript = "PID-5[1]=\"\"";
+      String messageFinal = "PID|||Q63W1^^^AIRA-TEST^MR||Holmes^Jeramiah^Z^IV^^^L|Monroe^Arden|20160626|M|||155 Lewis Cir^^Cadmus^MI^49221^USA^P||^PRN^PH^^^517^3004208|";
+      runTest(messageOriginal, modificationScript, messageFinal);
+    }
+    {
+      String messageOriginal = "PID|||Q63W1^^^AIRA-TEST^MR||Holmes^Jeramiah^Z^IV^^^L|Monroe^Arden|20160626|M|||155 Lewis Cir^^Cadmus^MI^49221^USA^P||^PRN^PH^^^517^3004208|";
+      String modificationScript = "PID-5.1=\"\"";
+      String messageFinal = "PID|||Q63W1^^^AIRA-TEST^MR||Holmes^Jeramiah^Z^IV^^^L|Monroe^Arden|20160626|M|||155 Lewis Cir^^Cadmus^MI^49221^USA^P||^PRN^PH^^^517^3004208|";
+      runTest(messageOriginal, modificationScript, messageFinal);
+    }
+
+  }
+
+  private void runTest(String messageOriginal, String modificationScript, String messageFinal) {
     ModifierService modifierService = new ModifierService();
     ModifyRequest modifyRequest = new ModifyRequest();
-    modifyRequest.setMessageOriginal("Hello!");
-    modifyRequest.setModificationScript("whatever");
+    modifyRequest.setMessageOriginal(messageOriginal);
+    modifyRequest.setModificationScript(modificationScript);
     modifierService.modify(modifyRequest);
-    assertEquals("Hello!", modifyRequest.getMessageFinal());
+    assertEquals(messageFinal, modifyRequest.getMessageFinal());
   }
 }
