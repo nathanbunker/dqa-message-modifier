2d80d9ca4fae924f6f28ee7a4d83b2af99de80bf
Youssef Bouij <youssef.bouij@telecomnancy.net>
2017-02-02 16:31:45 +0100

    Adding TruncFunction, ClearFunction, CleanFunction and Insert(before-after-first-last)Function

---
+++ src/main/java/org/immregistries/dqa/message_modifier/transform/TruncFunction.java
@@ -0,0 +1,13 @@
+package org.immregistries.dqa.message_modifier.transform;
+
+import java.io.IOException;
+
+import org.immregistries.dqa.message_modifier.ModifyRequest;
+
+public class TruncFunction implements CallFunction {
+	
+	public void doTransform(ModifyRequest modifyRequest, CallCommand callCommand) throws IOException{
+		System.out.println("Trunc Function TO DO");
+	}
+
+}
