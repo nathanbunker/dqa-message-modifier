167ec705dfc76b39fe60cba9c8ad2697216ebf61
Youssef Bouij <youssef.bouij@telecomnancy.net>
2017-02-17 16:47:36 +0100

    Fix Ampersand and Fix Escape in progress

---
+++ src/main/java/org/immregistries/dqa/message_modifier/transform/function/general/FixAmpersandFunction.java
@@ -0,0 +1,14 @@
+package org.immregistries.dqa.message_modifier.transform.function.general;
+
+import java.io.IOException;
+
+import org.immregistries.dqa.message_modifier.ModifyRequest;
+import org.immregistries.dqa.message_modifier.transform.CallCommand;
+
+public class FixAmpersandFunction implements CallFunction {
+	
+	public void doTransform(ModifyRequest modifyRequest, CallCommand callCommand) throws IOException{
+		System.out.println("FixAmpersand Function TO DO");
+	}
+
+}
---
+++ src/main/java/org/immregistries/dqa/message_modifier/transform/function/general/FixEscapeFunction.java
@@ -0,0 +1,14 @@
+package org.immregistries.dqa.message_modifier.transform.function.general;
+
+import java.io.IOException;
+
+import org.immregistries.dqa.message_modifier.ModifyRequest;
+import org.immregistries.dqa.message_modifier.transform.CallCommand;
+
+public class FixEscapeFunction implements CallFunction {
+	
+	public void doTransform(ModifyRequest modifyRequest, CallCommand callCommand) throws IOException{
+		System.out.println("FinxEscape Function TO DO");
+	}
+
+}
