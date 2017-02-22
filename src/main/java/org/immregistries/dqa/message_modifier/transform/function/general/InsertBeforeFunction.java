b9a2024dc4bc009811ca8c67094663b611555336
Youssef Bouij <youssef.bouij@telecomnancy.net>
2017-02-22 11:20:31 +0100

    Correction of InsertLast and InsertFirst, still a problem to fix

--- src/main/java/org/immregistries/dqa/message_modifier/transform/function/general/InsertBeforeFunction.java
+++ src/main/java/org/immregistries/dqa/message_modifier/transform/function/general/InsertBeforeFunction.java
@@ -8,25 +8,27 @@ import org.immregistries.dqa.message_modifier.ModifyRequest;
 import org.immregistries.dqa.message_modifier.transform.CallCommand;
 import org.immregistries.dqa.message_modifier.transform.ReferenceParsed;
 
-public class InsertBeforeFunction implements CallFunction {
-	
+public class InsertBeforeFunction implements CallFunction {	
 	public void doTransform(ModifyRequest modifyRequest, CallCommand callCommand) throws IOException{
+		System.out.println("InsertBefore Function TO DO");
 		String resultText = modifyRequest.getMessageFinal();
 		ReferenceParsed targetReference = callCommand.getTargetReference();
 		
 		String segID = callCommand.getParameterMap().get("SEGMENT ID");
-		// String segmentToAdd = segID + "|";
+		String segmentToAdd = segID + "|";
 
         BufferedReader inResult = new BufferedReader(new StringReader(resultText));
-        resultText = segID + "|";
+        resultText = "";
         String line = inResult.readLine();
         
         while(line != null){
+        	if(line.startsWith(targetReference.getSegmentName())){
+        		resultText += segmentToAdd + "\n";
+        	}
         	resultText += line + "\n";
         	line = inResult.readLine();
         }
         resultText = resultText.substring(0, resultText.length()-1);
 		modifyRequest.setMessageFinal(resultText);
 	}
-
 }
--- src/main/java/org/immregistries/dqa/message_modifier/transform/function/general/InsertFirstFunction.java
+++ src/main/java/org/immregistries/dqa/message_modifier/transform/function/general/InsertFirstFunction.java
@@ -1,14 +1,32 @@
 package org.immregistries.dqa.message_modifier.transform.function.general;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.StringReader;
 
 import org.immregistries.dqa.message_modifier.ModifyRequest;
 import org.immregistries.dqa.message_modifier.transform.CallCommand;
+import org.immregistries.dqa.message_modifier.transform.ReferenceParsed;
 
 public class InsertFirstFunction implements CallFunction {
 	
 	public void doTransform(ModifyRequest modifyRequest, CallCommand callCommand) throws IOException{
-		System.out.println("InsertFirst Function TO DO");
+		String resultText = modifyRequest.getMessageFinal();
+		ReferenceParsed targetReference = callCommand.getTargetReference();
+		
+		String segID = callCommand.getParameterMap().get("SEGMENT ID");
+		// String segmentToAdd = segID + "|";
+
+        BufferedReader inResult = new BufferedReader(new StringReader(resultText));
+        resultText = segID + "|";
+        String line = inResult.readLine();
+        
+        while(line != null){
+        	resultText += line + "\n";
+        	line = inResult.readLine();
+        }
+        resultText = resultText.substring(0, resultText.length()-1);
+		modifyRequest.setMessageFinal(resultText);
 	}
 
 }
